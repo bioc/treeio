e172d0def86d947e634e4aa2d0e0c8c0f6e090cf
Guangchuang Yu <guangchuangyu@gmail.com>
2018-11-29 23:59:03 +0800

    compatible with tibble 2.0.0

--- R/jplace.R
+++ R/jplace.R
@@ -91,7 +91,7 @@ extract.placement <- function(object, phylo) {
     }
     edgeNum.df <- attr(phylo, "edgeNum")
     place.df <- merge(place.df, edgeNum.df, by.x = "edge_num", by.y = "edgeNum")
-    as_data_frame(place.df)
+    as_tibble(place.df)
 }
 
 
